a237943560f741794d8a48494d23dafb1adaa04e
xiaoyaoyimei <1206902591@qq.com>
2019-03-26 17:31:19 +0800

    Updates

---
+++ precache-manifest.df704e841f34526403ad01b4cd1418fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2753f87d092ddb1717db533ecd2b38ea",
+    "url": "/rmall/static/media/cartempty.2753f87d.png"
+  },
+  {
+    "revision": "bb6483f1f82435402585821f47ba4f30",
+    "url": "/rmall/static/media/bg.bb6483f1.jpg"
+  },
+  {
+    "revision": "a79e4ac6ff41d3d17142",
+    "url": "/rmall/static/js/runtime~main.bb394ade.js"
+  },
+  {
+    "revision": "5439f8b6cd883fb1628e",
+    "url": "/rmall/static/js/main.14b63105.chunk.js"
+  },
+  {
+    "revision": "5fe398ebbd8c297b6f19",
+    "url": "/rmall/static/js/2.0896e3b7.chunk.js"
+  },
+  {
+    "revision": "5fe398ebbd8c297b6f19",
+    "url": "/rmall/static/css/2.5c5dc476.chunk.css"
+  },
+  {
+    "revision": "a27f0ca5f8df169348945c0cbefe1a84",
+    "url": "/rmall/index.html"
+  }
+];
